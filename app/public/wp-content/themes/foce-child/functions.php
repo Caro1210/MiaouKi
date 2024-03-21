dab4d9903e3467318be928970934f328b3a449e9
Caro1210 <c.delannoy12@gmx.com>
2024-03-21 23:56:16 +0100

    animation des flowers au scroll

--- app/public/wp-content/themes/foce-child/functions.php
+++ app/public/wp-content/themes/foce-child/functions.php
@@ -10,13 +10,15 @@ function theme_enqueue_styles() {
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), time());
 
     // script JavaScript activation
+    wp_enqueue_script( 'burger-js', get_theme_file_uri() . '/js/burger.js', array(), '1.0.0', true );
     wp_enqueue_script( 'script-js', get_theme_file_uri() . '/js/script.js', array('swiper'), '1.0.0', true );
     wp_enqueue_script( 'flower-animation-js', get_theme_file_uri() . '/js/flower-animation.js', array(), '1.0.0', true );
+    
 
    // Swiper JS & CSS
    wp_enqueue_script( 'swiper', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array(), '5.4.0', true );
    wp_enqueue_style( 'swiper-style', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css', array(), '5.4.0' );
-
+   
 }
 
 
--- app/public/wp-content/themes/foce-child/js/flower-animation.js
+++ app/public/wp-content/themes/foce-child/js/flower-animation.js
@@ -1,3 +1,29 @@
 //test
 console.log("Les flowers vont tourner plus vite !");
 
+document.addEventListener("DOMContentLoaded", function() {
+    let last_known_scroll_position = 0;
+    let ticking = false;
+  
+    function doSomething(scroll_pos) {
+        let speed = scroll_pos / 300; 
+        let elements = ['.orchid', '.sunflower'];
+        elements.forEach((selector) => {
+            let element = document.querySelector(selector);
+            if (element) {
+                element.style.animationDuration = `${speed}s`;
+            }
+        });
+    }
+    
+    window.addEventListener('scroll', function(e) {
+      last_known_scroll_position = window.scrollY;
+      if (!ticking) {
+        window.requestAnimationFrame(function() {
+          doSomething(last_known_scroll_position);
+          ticking = false;
+        });
+        ticking = true;
+      }
+    });
+});
